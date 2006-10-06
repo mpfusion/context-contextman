b5a772317e1156a41d1ed5ecab8b7e71a83240b0
vit <vit@08d8c78c-c36f-4a2b-834e-415383aec5ca>
2006-10-06 16:30:04 +0000

    Index sorting repair.
    git-svn-id: https://foundry.supelec.fr/svn/contextman@63 08d8c78c-c36f-4a2b-834e-415383aec5ca

--- context-beginners/cz/do-screen.bat
+++ context-beginners/cz/do-screen.bat
@@ -1,2 +1,5 @@
 @Echo OFF
-texexec --pdf --mode=screen --result=ma-cb-cz-screen.pdf ma-cb-cz
+texmfstart texexec --pdf --mode=screen ma-cb-cz
+cp ma-cb-cz.pdf ma-cb-cz-screen.pdf
+REM next line do not find .tuo in ConTeXt 2006.08.08 21:51
+REM texmfstart texexec --pdf --mode=screen --result=ma-cb-cz-screen.pdf ma-cb-cz
