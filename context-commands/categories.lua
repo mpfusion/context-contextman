4a64c3cd79b94cadf0758f32b0a35a44747e8cd5
peter <peter@08d8c78c-c36f-4a2b-834e-415383aec5ca>
2010-10-01 11:12:38 +0000

    ctx-ref.cld: print comments of commands
    git-svn-id: https://foundry.supelec.fr/svn/contextman@294 08d8c78c-c36f-4a2b-834e-415383aec5ca

---
+++ context-commands/categories.lua
@@ -0,0 +1,32 @@
+-- This table defines the order of the categories.
+return {
+    {
+        title = "Structure macros",
+        label = "struct",
+--      description = "Perhaps a lot of text here.",
+    },
+    {
+        title = "Layout",
+        label = "layout",
+    },
+    {
+        title = "Spacing",
+        label = "space",
+    },
+    {
+        title = "Graphic",
+        label = "graphic",
+    },
+    {
+        title = "Packaging",
+        label = "packaging",
+    },
+    {
+        title = "Interaction",
+        label = "interaction",
+    },
+    {
+        title = "System macros",
+        label = "system",
+    },
+}
